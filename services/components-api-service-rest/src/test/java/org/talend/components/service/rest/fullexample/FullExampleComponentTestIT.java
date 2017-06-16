2dae1ecd671854df3679cb78d16fd27aa2fbf2fd
Sebastien Gandon <sgandon@talend.com>
2017-06-16 14:00:41 +0200

    feat(TDKN-167):added some unit tests

--- services/components-api-service-rest/src/test/java/org/talend/components/service/rest/fullexample/FullExampleComponentTestIT.java
+++ services/components-api-service-rest/src/test/java/org/talend/components/service/rest/fullexample/FullExampleComponentTestIT.java
@@ -12,10 +12,13 @@
 // ============================================================================
 package org.talend.components.service.rest.fullexample;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.Response;
+import static com.jayway.restassured.RestAssured.given;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
 import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -26,19 +29,17 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.talend.components.service.rest.Application;
 import org.talend.components.service.rest.dto.PropertiesDto;
+import org.talend.components.service.spring.SpringTestApp;
 import org.talend.daikon.properties.test.PropertiesTestUtils;
 
-import static com.jayway.restassured.RestAssured.given;
-import static java.util.Collections.singletonList;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
+@SpringBootTest(classes = SpringTestApp.class, webEnvironment = RANDOM_PORT)
 @TestPropertySource(properties = { "server.contextPath=" })
 public class FullExampleComponentTestIT {
 
@@ -65,8 +66,7 @@ public class FullExampleComponentTestIT {
     }
 
     private ObjectNode getFullExampleDataStoreProperties() throws java.io.IOException {
-        return mapper.readValue(
-                IOUtils.toString(getClass().getResourceAsStream("fullexample_data_store_properties.json")),
+        return mapper.readValue(IOUtils.toString(getClass().getResourceAsStream("fullexample_data_store_properties.json")),
                 ObjectNode.class);
     }
 
@@ -117,6 +117,28 @@ public class FullExampleComponentTestIT {
 
     private ObjectNode getFileAsObjectNode(String file) throws java.io.IOException {
         return mapper.readerFor(ObjectNode.class).readValue(getClass().getResourceAsStream(file));
+    }
+
+    @Test
+    public void testAfterDatastoreCalled() throws java.io.IOException {
+        // given
+        PropertiesDto propertiesDto = new PropertiesDto();
+        propertiesDto.setProperties(getFileAsObjectNode("fullexample_dataset_properties.json"));
+        propertiesDto.setDependencies(singletonList(getFullExampleDataStoreProperties()));
+        String dataSetDefinitionName = "FullExampleDataset";
+
+        // when
+        Response response = given().content(propertiesDto).contentType(APPLICATION_JSON_UTF8_VALUE) //
+                .accept(APPLICATION_JSON_UTF8_VALUE) //
+                .expect().statusCode(200).log().ifError() //
+                .post("properties/{definitionName}", dataSetDefinitionName);
+
+        // then
+        ObjectNode fullexampleProperties = mapper.readerFor(ObjectNode.class).readValue(response.asInputStream());
+        assertNotNull(fullexampleProperties.get("jsonSchema"));
+        assertNotNull(fullexampleProperties.get("properties"));
+        assertNotNull(fullexampleProperties.get("uiSchema"));
+        assertEquals("bar", fullexampleProperties.get("properties").get("testAfterDatastoreTrigger").textValue());
     }
 
 }
